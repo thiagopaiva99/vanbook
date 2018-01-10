8fff2be15f0391c49e2123eb9e9db132b20b5f51
Thiago Galvani <thiagopaiva99@gmail.com>
2018-01-10 00:05:51 -0200

    Getting the scrollTop

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
+// modal functions
 $('a.btn').on('click', function(e) {
-	e.preventDefault();
-	$('.modal').addClass('modal-ativo');
-});
+	e.preventDefault()
+	$('.modal').addClass('modal-ativo')
+})
 
 $('.modal').on('click', function(modal) {
 	if (modal.target == this) {
-		$(this).removeClass('modal-ativo');
+		$(this).removeClass('modal-ativo')
 	}
-});
+})
+
+$(window).on('scroll', () => {
+	let windowTop = $(this).scrollTop()
+	console.log(windowTop)
+})
